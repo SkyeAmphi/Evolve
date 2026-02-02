33cfa43aceeaf57805e52ec1f12a57cdbf44ea94
Skye <xorngaming@gmail.com>
2026-02-02 18:19:42 +0000

    removing the debugging code for governors
    governors should be fine now

--- src/governor.js
+++ src/governor.js
@@ -793,9 +793,7 @@ function appointGovernor(){
     let govern = $(`<div id="candidates" class="governor candidates"></div>`);
     $('#r_govern1').append(govern);
 
-    console.log('holding election');
     if (!global.race.hasOwnProperty('governor') || !global.race.governor.hasOwnProperty('candidates') || global.race.governor.candidates.length === 0){
-        console.log('drafting candidates');
         global.race['governor'] = {
             candidates: genGovernor(10)
         };
@@ -809,10 +807,6 @@ function appointGovernor(){
         }
     }
 
-    console.log(global);
-    console.log(global.race);
-    console.log(global.race.governor);
-
     vBind({
         el: '#candidates',
         data: [],
@@ -822,7 +816,6 @@ function appointGovernor(){
                     let gov = global.race.governor.candidates[gi];
                     global.race.governor['g'] = gov;
                     global.race.governor.candidates = [];
-                    console.log(global.race.governor);
                     global.race.governor['tasks'] = {
                         t0: 'none', t1: 'none', t2: 'none', t3: 'none', t4: 'none', t5: 'none'
                     };
