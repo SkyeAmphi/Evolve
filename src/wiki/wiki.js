37c825acef33650c2f91295a9b95037a260ade30
Skye <xorngaming@gmail.com>
2026-05-30 17:02:37 -0400

    missing hybrid custom lab block

--- src/wiki/wiki.js
+++ src/wiki/wiki.js
@@ -76,7 +76,8 @@ function initPage(){
             submenu: [
                 { key: 'races' },
                 { key: 'traits' },
-                { key: 'custom' }
+                { key: 'custom' },
+                { key: 'hybrid_custom' }
             ]
         },
         {
